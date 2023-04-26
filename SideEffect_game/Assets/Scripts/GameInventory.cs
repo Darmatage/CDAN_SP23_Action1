6e59c7ca2547e160e286845bf24f805f818c3be1
StacyStacy2 <Scsantos@massart.edu>
2023-04-26 12:06:39 -0700

    fully functioning inventory

--- SideEffect_game/Assets/Scripts/GameInventory.cs
+++ SideEffect_game/Assets/Scripts/GameInventory.cs
@@ -13,13 +13,13 @@ public class GameInventory : MonoBehaviour {
       public static bool item2bool = false;
       public static bool item3bool = false;
       public static bool item4bool = false;
-      public static bool item5bool = false;
+     // public static bool item5bool = false;
 
       public static int item1num = 0;
       public static int item2num = 0;
       public static int item3num = 0;
       public static int item4num = 0;
-      public static int item5num = 0;
+     // public static int item5num = 0;
       //public static int coins = 0;
 
       [Header("Add item image objects here")]
@@ -27,7 +27,7 @@ public class GameInventory : MonoBehaviour {
       public GameObject item2image;
       public GameObject item3image;
       public GameObject item4image;
-      public GameObject item5image;
+     // public GameObject item5image;
       //public GameObject coinText;
 
       // Item number text variables. Comment out if each item is unique (1/2).
@@ -36,13 +36,13 @@ public class GameInventory : MonoBehaviour {
       public Text item2Text;
       public Text item3Text;
       public Text item4Text;
-      public Text item5Text;
+     // public Text item5Text;
 
       // Crafting buttons. Uncomment and add for each button:
       // public GameObject buttonCraft1; // weapon1 creation
  
       void Start(){
-            InventoryMenu.SetActive(true);
+            InventoryMenu.SetActive(false);
             //CraftMenu.SetActive(false);
             InventoryDisplay();
       }
@@ -52,7 +52,7 @@ public class GameInventory : MonoBehaviour {
             if (item2bool == true) {item2image.SetActive(true);} else {item2image.SetActive(false);}
             if (item3bool == true) {item3image.SetActive(true);} else {item3image.SetActive(false);}
             if (item4bool == true) {item4image.SetActive(true);} else {item4image.SetActive(false);}
-            if (item5bool == true) {item5image.SetActive(true);} else {item5image.SetActive(false);}
+       //     if (item5bool == true) {item5image.SetActive(true);} else {item5image.SetActive(false);}
 
             //Text coinTextB = coinText.GetComponent<Text>();
             //coinTextB.text = ("COINS: " + coins);
@@ -70,8 +70,8 @@ public class GameInventory : MonoBehaviour {
             Text item4TextB = item4Text.GetComponent<Text>();
             item4TextB.text = ("" + item4num);
 
-            Text item5TextB = item5Text.GetComponent<Text>();
-            item5TextB.text = ("" + item5num);
+          //  Text item5TextB = item5Text.GetComponent<Text>();
+           // item5TextB.text = ("" + item5num);
       }
 
       public void InventoryAdd(string item){
@@ -80,7 +80,7 @@ public class GameInventory : MonoBehaviour {
             else if (foundItemName == "item2") {item2bool = true; item2num ++;}
             else if (foundItemName == "item3") {item3bool = true; item3num ++;}
             else if (foundItemName == "item4") {item4bool = true; item4num ++;}
-            else if (foundItemName == "item5") {item5bool = true; item5num ++;}
+          //  else if (foundItemName == "item5") {item5bool = true; item5num ++;}
             else { Debug.Log("This item does not exist to be added"); }
             InventoryDisplay();
 
@@ -111,11 +111,13 @@ public class GameInventory : MonoBehaviour {
                   if (item4num <= 0) { item4bool =false; }
                     // Add any other intended effects
             }
-            else if (itemRemove == "item5") {
+          /*
+		   else if (itemRemove == "item5") {
                   item5num -= num;
                   if (item5num <= 0) { item5bool =false; }
                     // Add any other intended effects
             }
+			*/
             else { Debug.Log("This item does not exist to be removed"); }
             InventoryDisplay();
       }
@@ -142,13 +144,33 @@ public class GameInventory : MonoBehaviour {
             item2bool = false;
             item3bool = false;
             item4bool = false;
-            item5bool = false;
+          //  item5bool = false;
 
             item1num = 0; // object name
             item2num = 0; // object name
             item3num = 0; // object name
             item4num = 0; // object name
-            item5num = 0; // object name
+          //  item5num = 0; // object name
       }
 
+	public void ButtonItem1(){
+		//ju-lee, do the thing
+		InventoryRemove("item1", 1);
+	}
+
+	public void ButtonItem2(){
+		//ju-lee, do the thing
+		InventoryRemove("item2", 1);
+	}
+
+	public void ButtonItem3(){
+		//ju-lee, do the thing
+		InventoryRemove("item3", 1);
+	}
+
+	public void ButtonItem4(){
+		//ju-lee, do the thing
+		InventoryRemove("item4", 1);
+	}
+	
 }
---
+++ SideEffect_game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,70 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour{
+
+      public GameHandler gameHandler;
+      //public playerVFX playerPowerupVFX;
+      public bool isHealthPickUp = true;
+      public bool isKey = false;
+	  public bool isScrewdriver = false;
+	  public bool isMachinePart1 = false;
+	  public bool isMachinePart2 = false;
+	  //public bool isMachinePart3 = false;
+	  
+
+      public int healthBoost = 50;
+
+
+      void Start(){
+            gameHandler = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+            //playerPowerupVFX = GameObject.FindWithTag("Player").GetComponent<playerVFX>();
+      }
+
+      public void OnTriggerEnter2D (Collider2D other){
+            if (other.gameObject.tag == "Player"){
+                  GetComponent<Collider2D>().enabled = false;
+                  //GetComponent<AudioSource>().Play();
+                  StartCoroutine(DestroyThis());
+
+                  if (isHealthPickUp == true) {
+                        gameHandler.playerGetHit(healthBoost * -1);
+                        //playerPowerupVFX.powerup();
+                  }
+
+
+                  if (isKey == true) {
+                        gameHandler.GetComponent<GameInventory>().InventoryAdd("item1");
+                        //playerPowerupVFX.powerup();
+                  }
+
+                  if (isScrewdriver == true) {
+                        gameHandler.GetComponent<GameInventory>().InventoryAdd("item2");
+                        //playerPowerupVFX.powerup();
+                  }
+				  
+                  if (isMachinePart1 == true) {
+                        gameHandler.GetComponent<GameInventory>().InventoryAdd("item3");
+                        //playerPowerupVFX.powerup();
+                  }
+
+                  if (isMachinePart2 == true) {
+                        gameHandler.GetComponent<GameInventory>().InventoryAdd("item4");
+                        //playerPowerupVFX.powerup();
+                  }				  
+
+
+                 // if (isSpeedBoostPickUp == true) {
+                 //       other.gameObject.GetComponent<PlayerMove>().speedBoost(speedBoost, speedTime);
+                        //playerPowerupVFX.powerup();
+                 // }
+            }
+      }
+
+      IEnumerator DestroyThis(){
+            yield return new WaitForSeconds(0.3f);
+            Destroy(gameObject);
+      }
+
+}
