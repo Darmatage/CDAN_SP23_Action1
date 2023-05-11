abde7398fe048ef167d1eda8ec123a87ed4f0aa6
Darmatage <JasonWiserArt@gmail.com>
2023-05-10 23:15:32 -0400

    Moveable obstacles now allow jumping in the future!

--- SideEffect_game/Assets/Scripts/Obstacle_TimeState.cs
+++ SideEffect_game/Assets/Scripts/Obstacle_TimeState.cs
@@ -36,11 +36,14 @@ public class Obstacle_TimeState : MonoBehaviour{
 	
 	//make moveable objects stop as soon as they are released
 	void OnCollisionExit2D(Collision2D colExt){
-		gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
-		if ((colExt.gameObject.GetComponent<Rigidbody2D>() != null) && (colExt.gameObject.GetComponent<Rigidbody2D>().bodyType ==RigidbodyType2D.Dynamic)){
-			colExt.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+		if (GameHandler_TimeStates.isPast == true){
+			gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			if ((colExt.gameObject.GetComponent<Rigidbody2D>() != null) && (colExt.gameObject.GetComponent<Rigidbody2D>().bodyType ==RigidbodyType2D.Dynamic))
+			{
+				colExt.gameObject.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			}
 		}
-    }
+	}
 	
 }
 
